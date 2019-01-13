3ea6f92ed3d0981099c106147b26530bc4de3cff
Sarah Lee <sarahselee223@gmail.com>
2019-01-13 01:05:11 -0800

    install npm dependencies and create MVC folders

---
+++ README.md
@@ -0,0 +1,23 @@
+
+```
+npm install
+```
+
+```
+<!-- createdb task_management_db
+```
+
+```
+npm run latest
+npm run seed
+```
+
+```
+cp .env.example .env
+``` -->
+
+## To Run Server
+
+```
+npm run dev
+```
---
+++ src/app.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+const authController = require('./controllers/auth')
+
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').load()
+}
+
+const app = express()
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+//////////////////////////////////////////////////////////////////////////////
+// Routes
+//////////////////////////////////////////////////////////////////////////////
+
+app.use('/auth', require('./routes/auth'))
+app.use('/users', require('./routes/users'))
+
+//////////////////////////////////////////////////////////////////////////////
+// example routes, not part of an organized application
+//////////////////////////////////////////////////////////////////////////////
+
+app.get('/protected',
+        authController.isAuthenticated,
+        function(req, res, next){ res.send({ id: req.claim.id, message: "For authenticated eyes only" }) })
+
+app.get('/protected/:userId',
+        authController.isAuthenticated,
+        authController.isSelf,
+        function(req, res, next){ res.send({ id: req.claim.id, message: "For your eyes only"}) })
+
+//////////////////////////////////////////////////////////////////////////////
+// Default Route
+//////////////////////////////////////////////////////////////////////////////
+
+app.use(function(req, res, next){
+  next({status: 404, message: 'Route not found' })
+})
+
+//////////////////////////////////////////////////////////////////////////////
+// Error Handling
+//////////////////////////////////////////////////////////////////////////////
+
+app.use(function(err, req, res, next){
+  console.log(err)
+  const errorMessage = {}
+
+  if(process.env.NODE_ENV !== 'production' && err.stack)
+    errorMessage.stack = err.stack
+
+  errorMessage.status = err.status || 500
+  errorMessage.message = err.message || 'Internal Server Error'
+
+  res.status(errorMessage.status).send(errorMessage)
+})
+
+//////////////////////////////////////////////////////////////////////////////
+// Starting Server
+//////////////////////////////////////////////////////////////////////////////
+
+const port = process.env.PORT || 5000
+
+app.listen(port, function(){
+  console.log(`Listening on port ${port}`)
+})
